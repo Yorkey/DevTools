3bbf2634bdd43081f6a22cdc29f956bff94cdda3
wangyu <815776870@qq.com>
2017-01-20 18:10:30 +0800

    developing

--- app/src/main/java/cn/helloyy/devtools/controller/HomeViewController.kt
+++ app/src/main/java/cn/helloyy/devtools/controller/HomeViewController.kt
@@ -1,5 +1,6 @@
 package cn.helloyy.devtools.controller
 
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
 import android.view.LayoutInflater
@@ -9,6 +10,9 @@ import cn.helloyy.devtools.R
 import cn.helloyy.devtools.base.BaseController
 import org.jetbrains.anko.appcompat.v7.titleResource
 import org.jetbrains.anko.find
+import kotlinx.android.synthetic.main.controller_home.view.*
+import org.jetbrains.anko.image
+import org.jetbrains.anko.imageResource
 
 /**
  * Created by wangyu on 2017/1/3.
@@ -23,7 +27,14 @@ class HomeViewController(args: Bundle?) : BaseController(args) {
     override fun onViewBound(view: View) {
         super.onViewBound(view)
 
-        val toolbar = view.find<Toolbar>(R.id.toolbar)
-        toolbar.titleResource = R.string.title_controller_home
+        view.headerIcon.imageResource = when(Build.VERSION.SDK_INT) {
+            24 -> R.drawable.nougat
+            23 -> R.drawable.marshmallow
+            21,22 -> R.drawable.lollipop
+            20 -> R.drawable.kitkat
+            else -> R.drawable.default_android
+        }
+
+        view.headerTitle.text = android.os.Build.MODEL
     }
 }
